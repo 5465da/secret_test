f42ef5d25c0018c8a2120fdb3d28cc380fed6b77
hassan dohlan <h.bin.mohd.dohlan@techpass.gov.sg>
2026-01-02 11:14:54 +0800

    fix

--- src/main/java/sg/gov/moe/masking/config/EmailConfig.java
+++ src/main/java/sg/gov/moe/masking/config/EmailConfig.java
@@ -5,6 +5,7 @@ import java.util.Properties;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
@@ -13,6 +14,7 @@ import sg.gov.moe.masking.service.SecretWrapperService;
 import sg.gov.moe.masking.util.common.ServiceUtil;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 public class EmailConfig {
 
 	@Value("${smtp.host}")
