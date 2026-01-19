3ede143912a71c17a7cd2082af46aa0412e89646
hassan dohlan <h.bin.mohd.dohlan@techpass.gov.sg>
2026-01-19 13:12:18 +0800

    use common-emails

--- src/main/java/sg/gov/moe/masking/service/EmailService.java
+++ src/main/java/sg/gov/moe/masking/service/EmailService.java
@@ -1,14 +1,14 @@
 package sg.gov.moe.masking.service;
 
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.HtmlEmail;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
-import javax.mail.internet.MimeMessage;
+
+import sg.gov.moe.masking.model.SecretManagerResponseBody;
 
 @Service
 public class EmailService {
@@ -18,12 +18,14 @@ public class EmailService {
 	@Value("${email.from}")
 	private String fromEmail;
 
-	private JavaMailSender javaMailSender;
+	@Value("${smtp.host}")
+	private String smtpHost;
+
+	@Value("${smtp.port}")
+	private int smtpPort;
 
 	@Autowired
-	public EmailService(JavaMailSender javaMailSender) {
-		this.javaMailSender = javaMailSender;
-	}
+	private SecretWrapperService secretWrapperService;
 
     /**
      * Send Email
@@ -33,19 +35,29 @@ public class EmailService {
      */
 	public void sendEmail(String recipient, String subject, String content) {
 		try {
-			MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-			MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
-			if(recipient.contains("|")) {
-				String[] recipients = recipient.split("\\|");
-				helper.setTo(recipients);
-			} else {
-				helper.setTo(recipient);
+			logger.info("Preparing to send email to {}", recipient);
+			SecretManagerResponseBody secret = secretWrapperService.getSecretValue("aws_smtp_credentials");
+			
+			HtmlEmail email = new HtmlEmail();
+			email.setHostName(smtpHost);
+			email.setSmtpPort(smtpPort);
+			if (secret != null) {
+				email.setAuthentication(secret.getAccessKeyId(), secret.getSecretAccessKey());
 			}
-			helper.setFrom(fromEmail);
-			helper.setSubject(subject);
-			helper.setText(content, true);
-			javaMailSender.send(mimeMessage);
-
+			email.setSSLOnConnect(true);
+			
+			email.setFrom(fromEmail);
+			if(recipient.contains("|")){
+				String [] toMail = recipient.split("\\|");
+				for (String to : toMail) {
+					email.addTo(to);
+				}
+			} else {
+				email.addTo(recipient);
+			}
+			email.setSubject(subject);
+			email.setHtmlMsg(content);
+			email.send();
 			logger.info("Email sent successfully to: {}", recipient);
 		} catch (Exception e) {
 			logger.error("Failed to send email to {}: {}", recipient, e.getMessage(), e);
